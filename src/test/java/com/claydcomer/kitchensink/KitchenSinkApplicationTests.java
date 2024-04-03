f87b56d708f7b223cf441f863722007c4c94e446
Clay Comer <7101603+claydcomer@users.noreply.github.com>
2024-04-03 11:42:03 -0400

    Adding base files from spring initializer

---
+++ src/test/java/com/claydcomer/kitchensink/KitchenSinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claydcomer.kitchensink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenSinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
