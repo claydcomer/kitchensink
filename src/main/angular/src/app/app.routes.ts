e0ffff38cb9eabe20ea857ac8d14bf0c0cca7f53
Clay Comer <7101603+claydcomer@users.noreply.github.com>
2024-04-05 16:56:50 -0400

    Add hello world functionality

--- src/main/angular/src/app/app.routes.ts
+++ src/main/angular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {HelloWorldComponent} from "./component/hello-world/hello-world.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HelloWorldComponent},
+  {path: 'hello-world', component: HelloWorldComponent}
+];
---
+++ src/main/java/com/claydcomer/kitchensink/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.claydcomer.kitchensink.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello-world")
+    public Map<String,Object> home() {
+        Map<String,Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+}
